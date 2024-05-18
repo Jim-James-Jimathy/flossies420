f4b571a41b859f825a736d6b3006b96ec7abe418
James Alan Mikal Kirkwood <139880864+Jim-James-Jimathy@users.noreply.github.com>
2024-05-18 10:33:51 -0400

    finished layout for mailing list

--- src/app/_components/MailingList/index.tsx
+++ src/app/_components/MailingList/index.tsx
@@ -5,33 +5,22 @@ import classes from './index.module.scss'
 
 const MailingList = () => {
   return (
-    <div className={classes.container}>
+    <div className={classes.mailingList}>
       <div className={classes.wrapper}>
-        <div>
-          <div className={classes.text}>
-            <h2>Join our mailing list</h2>
-            <p>here are a few reasons why you should!</p>
+        <div className={classes.text}>
+          <div className={classes.content}>
+            <h2>Subscribe to our mailing list</h2>
             <ul>
               <li>Join our inner circle of high rollers and elevate your experience</li>
               <li>Sign up now to unlock exclusive discounts on top-notch products</li>
               <li>stay in the loop with upcoming events and where we're rolling up</li>
-              <li>Don't miss out on the buzz</li>
             </ul>
           </div>
         </div>
         <div className={classes.btn}>
-          <div className={classes.imageWrap}>
-            <div>
-              <Image
-                src="/assets/images/book-toad.jpg"
-                alt="image of a toad sitting on a mushroom reading a book"
-                fill
-              />
-            </div>
-          </div>
-          <div className={classes.link}>
-            <p>Click here to join</p>
-            <Link href="/signup">JOIN</Link>
+          <div className={classes.btnInner}>
+            <h4>subscribe today and keep your stash stocked!</h4>
+            <Link href="/https://sitch.app/s/6EDoz0X">subscribe</Link>
           </div>
         </div>
       </div>
