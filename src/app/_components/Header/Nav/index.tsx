4f06f9582753964372ff8e114e1c95882b1a2b3a
James Alan Mikal Kirkwood <139880864+Jim-James-Jimathy@users.noreply.github.com>
2024-05-20 02:10:39 -0400

    okay, now the front page is done.

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -35,7 +35,7 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = () => {
       )}
       {!user && (
         <Link href="/login" className={classes.loginBtn}>
-          Login
+          <span>Login</span>
         </Link>
       )}
     </nav>
