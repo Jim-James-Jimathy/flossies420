4a3fcc6cb55dd3e66ef43f8521093ad5dfdc48c0
James Alan Mikal Kirkwood <139880864+Jim-James-Jimathy@users.noreply.github.com>
2024-05-21 22:05:04 -0400

    artists page reformatted and styled

--- src/app/(pages)/artists/ArtistCard/index.tsx
+++ src/app/(pages)/artists/ArtistCard/index.tsx
@@ -6,10 +6,9 @@ import classes from './index.module.scss'
 const ArtistCard = ({ data }) => {
   return (
     <Link href={data.href} className={classes.artistCard} rel="noopener noreferrer" target="_blank">
-      <Image src={data.src} alt={data.title} fill />
+      <Image src={data.src} alt={data.title} width={350} height={350} />
       <div className={classes.artistText}>
         <h3>{data.title}</h3>
-        <p>{data.desc}</p>
       </div>
     </Link>
   )
